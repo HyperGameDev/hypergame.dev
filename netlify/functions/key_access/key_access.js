3e2c60fa2125a253838515fa6107988ba87649e2
HyperGameDev <147095077+HyperGameDev@users.noreply.github.com>
2024-11-30 21:01:15 -0600

    netlify folder and api func added

---
+++ netlify/functions/key_access/key_access.js
@@ -0,0 +1,25 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// functions/get-api-data.js
+const axios = require('axios');
+
+exports.handler = async function() {
+  const apiKey = process.env.HGD_YT_API;
+
+
+  try {
+    // Use the API key to make a request to the YouTube API
+    const response = await axios.get(https://www.googleapis.com/youtube/v3/some-endpoint?key=${apiKey});
+
+
+return {
+  statusCode: 200,
+  body: JSON.stringify(response.data)
+};
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error fetching data from YouTube API' })
+    };
+  }
+}
