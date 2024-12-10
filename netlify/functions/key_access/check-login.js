ffcfd3cfc28d8be18607095e4a06fd8b2c362001
HyperGameDev <147095077+HyperGameDev@users.noreply.github.com>
2024-12-10 15:07:02 -0600

    fb env var update for check-login.js

--- netlify/functions/key_access/check-login.js
+++ netlify/functions/key_access/check-login.js
@@ -2,18 +2,18 @@ const admin = require('firebase-admin');
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        type: process.env.FIREBASE_TYPE,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+        type: process.env.HGD_FB1_TYPE,
+        project_id: process.env.HGD_FB1_PROJECT_ID,
+        private_key_id: process.env.HGD_FB1_PRIVATE_KEY_ID,
+        private_key: process.env.HGD_FB1_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.HGD_FB1_CLIENT_EMAIL,
+        client_id: process.env.HFD_FB1_CLIENT_ID,
+        auth_uri: process.env.HFD_FB1_AUTH_URI,
+        token_uri: process.env.HFD_FB1_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.HGD_FB1_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.HGD_FB1_CLIENT_X509_CERT_URL
     }),
-    databaseURL: 'https://your-firebase-project-id.firebaseio.com'  // Replace this with your actual project ID
+    databaseURL: process.env.HGD_FB1_RTDB1  // Use the environment variable for the database URL
 });
 
 exports.handler = async function(event, context) {
